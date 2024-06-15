e6806db48854634f051d620f55adeeaf0e2332f9
Piipip <philipkorans@yahoo.com>
2024-06-15 10:38:52 +0000

    Hot fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,5 @@
-# installs flask from pip3.
-package { 'python3-pip':
-  ensure => installed,
+# Install flask
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
-
-exec { 'install-flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  path    => ['/usr/bin', '/usr/sbin'],
-  creates => '/usr/local/bin/flask',
-}
-
